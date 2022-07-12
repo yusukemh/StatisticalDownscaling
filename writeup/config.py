5e5b0ddaeb5a5d444122f89ce086d008d8c15ae0
yusukemh <yusukemh@hawaii.edu>
2022-07-12 09:09:47 +0000

    grid concatenation complete

---
+++ writeup/config.py
@@ -0,0 +1,37 @@
+FILE_NAMES = [
+    "air.2m.mon.mean.regridded.nc",
+    "air.1000-500.mon.mean.nc",
+    "hgt500.mon.mean.nc",
+    "hgt1000.mon.mean.nc",
+    "omega500.mon.mean.nc",
+    "pottmp.1000-500.mon.mean.nc",
+    "pottmp.1000-850.mon.mean.nc",
+    "pwtr.mon.mean.nc",
+    "shum_x_uwnd.700.mon.mean.nc",
+    "shum_x_uwnd.925.mon.mean.nc",
+    "shum_x_vwnd.700.mon.mean.nc",
+    "shum_x_vwnd.925.mon.mean.nc",
+    "shum700.mon.mean.nc",
+    "shum925.mon.mean.nc",
+    "skt.mon.mean.regridded.nc",
+    "slp.mon.mean.nc"
+]
+
+DF_LABELS = [
+    "air2m",
+    "air1000_500",
+    "hgt500",
+    "hgt1000",
+    "omega500",
+    "pottemp1000-500",
+    "pottemp1000-850",
+    "pr_wtr",
+    "shum-uwnd-700",
+    "shum-uwnd-925",
+    "shum-vwnd-700",
+    "shum-vwnd-950",
+    "shum700",
+    "shum925",
+    "skt",
+    "slp"
+]
